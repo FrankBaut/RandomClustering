cba30579fe3787be315127e0f92d65ecb3617263
FrankBaut <48511051+FrankBaut@users.noreply.github.com>
2019-08-10 14:22:14 -0500

    Update matrix_cl.R
    update

--- R/matrix_cl.R
+++ R/matrix_cl.R
@@ -1,4 +1,4 @@
-#' matrix_cl
+#' clusters_matrix
 #'
 #' @param ncell
 #' @param prob_vector
@@ -7,7 +7,7 @@
 #' @export
 #'
 #' @examples
-matrix_cl <- function(prob, ncell) {
+clusters_matrix <- function(ncell=10,prob = 0.5) {
   rbinom(n = ncell^2, size = 1, prob = prob) %>%
     as.logical(.) %>%
     matrix(data = ., nrow = ncell, ncol = ncell) %>%
