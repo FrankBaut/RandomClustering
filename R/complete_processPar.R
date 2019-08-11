a620128d08516aee3cc3f379bbd8f6818e8f7d8b
FrankBaut <48511051+FrankBaut@users.noreply.github.com>
2019-08-11 02:50:47 -0500

    Update complete_processPar.R
    update

--- R/complete_processPar.R
+++ R/complete_processPar.R
@@ -1,3 +1,15 @@
+#' complete_processPar
+#'
+#' @param n
+#'
+#' @param prob_vec
+#'
+#' @param iterations
+#'
+#' @return
+#' @export
+#'
+#' @examples
 complete_processPar<-function(n,prob_vec,iterations){
   clustering<-foreach(i=prob_vec) %do%{
     data<-clusters_matrix(n,i)
