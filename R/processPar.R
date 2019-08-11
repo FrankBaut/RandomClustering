dedcd2604302b14cb922c8f747033ee532f64e90
Fco MB <mbautistafran@gmail.com>
2019-08-11 02:35:42 -0500

    update

---
+++ R/processPar.R
@@ -0,0 +1,8 @@
+processPar<-function(iterations,data){
+  cluster<-foreach(i=seq_len(iterations)) %dopar% {
+    data<-proceso(data)
+    caso<-data %>% as.vector()%>% na.omit() %>% plyr::count()
+    max(caso$freq,na.rm = T)/sum(caso$freq,na.rm = T)#
+  }
+  return(mean(unlist(cluster)))
+}
