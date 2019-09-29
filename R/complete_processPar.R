9de8035c315a187af63eb923366b2c922f0ed0a7
FrankBaut <48511051+FrankBaut@users.noreply.github.com>
2019-09-29 11:40:31 -0500

    Update complete_processPar.R

--- R/complete_processPar.R
+++ R/complete_processPar.R
@@ -26,6 +26,7 @@ complete_processPar<-function(n,prob_vec,iterations,l,pb){
   clustering<-foreach(i=prob_vec,j = icount(length(prob_vec))) %do%{
     data<-clusters_matrix(n,i)
     setTxtProgressBar(pb, j)
+    gc()
     data<- processPar(iterations,data)
     data
   }
