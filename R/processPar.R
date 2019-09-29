b3ae2fcef6ff81c3343b9b85ad7d36fac2aebf84
FrankBaut <48511051+FrankBaut@users.noreply.github.com>
2019-09-29 11:39:33 -0500

    Update processPar.R

--- R/processPar.R
+++ R/processPar.R
@@ -12,6 +12,7 @@ processPar<-function(iterations,data){
   cluster<-foreach(i=seq_len(iterations)) %dopar% {
     data<-proceso(data)
     caso<-data %>% as.vector()%>% na.omit() %>% plyr::count()
+    gc()
     max(caso$freq,na.rm = T)/sum(caso$freq,na.rm = T)
    #length(caso$freq)
 
